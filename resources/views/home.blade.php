5bd280b458b0d1c5266db39c35cadbbb567697e4
alessiologozzo <alessio.logozzo@libero.it>
2023-05-24 18:11:02 +0200

    added main tag in home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,36 +5,39 @@
 @endsection
 
 @section('content')
-    <div class="al-jumbo"></div>
+    <main>
+        <div class="al-jumbo"></div>
 
-    <div class="container al-cur-ser position-relative py-5">
+        <div class="container al-cur-ser position-relative py-5">
 
-        <div class="row">
+            <div class="row">
 
-            @for ($i = 0; $i < count($comics); $i++)
-                <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 py-4">
-                    <a href="{{route('show', $i)}}" class="al-card d-flex flex-column gap-2">
-                        <img src="{{ $comics[$i]['thumb'] }}" alt="err">
-                        <strong class="text-center">{{ $comics[$i]['title'] }}</strong>
-                    </a>
-                </div>
-            @endfor
+                @for ($i = 0; $i < count($comics); $i++)
+                    <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 py-4">
+                        <a href="{{ route('show', $i) }}" class="al-card d-flex flex-column gap-2">
+                            <img src="{{ $comics[$i]['thumb'] }}" alt="err">
+                            <strong class="text-center">{{ $comics[$i]['title'] }}</strong>
+                        </a>
+                    </div>
+                @endfor
+
+            </div>
 
         </div>
 
-    </div>
+        <div class="al-bg-blue">
+            <div class="container d-flex justify-content-center align-items-center">
 
-    <div class="al-bg-blue">
-        <div class="container d-flex justify-content-center align-items-center">
-
-            <div class="row al-categories">
-                @foreach ($categories as $category)
-                    <div class="col-12 col-sm-6 col-md-4 al-col-lg py-3 d-flex justify-content-center align-items-center gap-2">
-                        <img src="{{ Vite::asset($category['source']) }}" alt="err">
-                        <span class="text-uppercase">{{ $category['name'] }}</span>
-                    </div>
-                @endforeach
+                <div class="row al-categories">
+                    @foreach ($categories as $category)
+                        <div
+                            class="col-12 col-sm-6 col-md-4 al-col-lg py-3 d-flex justify-content-center align-items-center gap-2">
+                            <img src="{{ Vite::asset($category['source']) }}" alt="err">
+                            <span class="text-uppercase">{{ $category['name'] }}</span>
+                        </div>
+                    @endforeach
+                </div>
             </div>
         </div>
-    </div>
+    </main>
 @endsection
